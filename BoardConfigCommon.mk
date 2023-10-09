2471f18d95fb219289498706a271f05f79868ca7
LuK1337 <priv.luk@gmail.com>
2023-10-09 12:26:55 +0200

    rosemary: Include device FCM from vendor/lineage
    Sadly we aren't able to include it globally without breaking other
    devices ;((((
    
    Change-Id: Ia29d6fb047a09a2b4767312cb2a3252c87095723

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -165,7 +165,9 @@ TARGET_VIBRATOR_SUPPORTS_EFFECTS := true
 # VINTF
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE += $(COMMON_PATH)/compatibility_matrix.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(COMMON_PATH)/framework_compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+	$(COMMON_PATH)/framework_compatibility_matrix.xml \
+         vendor/lineage/config/device_framework_matrix.xml
 
 # VNDK
 BOARD_VNDK_VERSION := current
